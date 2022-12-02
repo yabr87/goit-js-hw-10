f49a444d0e4403b18dfaf46f025fb9f004da94e7
yabr87 <108272773+yabr87@users.noreply.github.com>
2022-12-03 00:26:19 +0200

    start

--- src/js/refs.js
+++
@@ -1,3 +0,0 @@
-export const searchBox = document.querySelector('#search-box');
-export const countryList = document.querySelector('.country-list');
-export const countryInfo = document.querySelector('.country-info');
